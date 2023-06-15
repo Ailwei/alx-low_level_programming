c287d50633f4d232dbe17c3919cddbc5d89110c0
Ailwei <Ailweimaemu@gmail.com>
2023-06-15 05:18:20 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  * main - prints "Programming is like building a multilingual
  * puzzle, Followed by a newline."
  *
- * Return: Always 0
+ * Return: Always 0.
  */
 {
 	puts("Programming is like building a multilingual puzzle");
-	return (0);
+	return(0);
 }
