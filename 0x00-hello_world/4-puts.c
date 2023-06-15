dd32447d7dd7d140ed9338734eeaa65ed00c1207
Ailwei <Ailweimaemu@gmail.com>
2023-06-15 05:24:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 /**
- * main - prints "Programming is like building a multilingual
- * puzzle, Followed by a newline."
+ * main - prints "Programming is like building a multilingual puzzle, Followed by a newline."
  *
  * Return: Always 0.
  */
 {
 	puts("Programming is like building a multilingual puzzle");
-	return(0);
+	return (0);
 }
