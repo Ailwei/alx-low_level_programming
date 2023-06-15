0b814a52c9a94511e6459aa85bf81ab574ac908d
Ailwei <Ailweimaemu@gmail.com>
2023-06-15 15:06:38 -0700

    8. UNIX is basically a simple operating system

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,7 +4,8 @@
  *
  * Return: Always 0 (Success)
  */
-int main(void){
+int main(void)
+{
 const char message[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 write(2, message, sizeof(message) - 1);
 return (1);
