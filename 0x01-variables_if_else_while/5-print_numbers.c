515ae6950f3366e4f2a6d8d11e1609a809b1afb2
Ailwei <Ailweimaemu@gmail.com>
2023-06-17 11:30:40 -0700

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -Print all single digit of bas 10
+ * starting from 0, followed by a new line
+ *
+ * Return:Always 0 successful
+ */
+
+int main(void)
+{
+	int number;
+	for (number = 0; number < 10; number++)
+	{
+		putchar('0' + number);
+	}
+	putchar('\n');
+	return (0);
+} 
+
