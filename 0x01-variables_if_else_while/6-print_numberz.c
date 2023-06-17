ccb2fb7beab32712bf6661f5ec2fdda54a79b647
Ailwei <Ailweimaemu@gmail.com>
2023-06-17 12:31:58 -0700

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Print all single numbers of 10 starting from 0,
+ * followed by a new line
+ *
+ * return:Always 0 success
+ */
+int main(void)
+{
+	int number;
+	
+	for(number = 0;number < 10; number++)
+	{
+		putchar(number + '0');
+		if (number != 9)
+			putchar(',');
+	}
+	putchar('\n');
+	return (0);
+}
