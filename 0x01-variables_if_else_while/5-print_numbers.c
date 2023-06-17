c3b83262631e71ee00cac53a536807ce8c503acd
Ailwei <Ailweimaemu@gmail.com>
2023-06-17 11:42:13 -0700

    5. Numbers

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -9,12 +9,13 @@
 int main(void)
 {
 	int number;
-	for (number = 0; number < 10; number++)
+        for (number = 0; number < 10; number++)
 
-	{
-		putchar('0' + number);
-	}
-	putchar('\n');
-	return (0);
-} 
+        {
+                putchar('0' + number);
+        }
+        putchar('\n');
+        return (0);
 
+}
+	
