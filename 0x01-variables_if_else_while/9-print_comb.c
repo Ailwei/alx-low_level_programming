d5c64aae116e89a957d5ffe1328be27fdfb3fe36
Ailwei <Ailweimaemu@gmail.com>
2023-06-18 04:30:54 -0700

    9. Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -12,6 +12,7 @@ int main(void)
 	for (single_digit_combo = 0; single_digit_combo <= 9; single_digit_combo++)
 	{
 		putchar(single_digit_combo + '0');
+		if (single_digit_combo != 9)
 		putchar(',');
 	}
 	putchar('\n');
