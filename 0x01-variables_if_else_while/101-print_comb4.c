efa21d21a697e14c996ee5d9aa9820838dc5a249
Ailwei <Ailweimaemu@gmail.com>
2023-06-18 08:25:50 -0700

    11. The success combination in business is: Do what you do better... and: do more of what you do..

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,4 +1,10 @@
 #include <stdio.h>
+/**
+ * main - prints all possible combinations of single-digit numbers.
+ * Numbers must be separated by ,, followed by a space
+ *
+ * Return: Always 0 Success
+ */
 
 int main(void)
 {
