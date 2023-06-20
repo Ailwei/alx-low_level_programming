bcf08e56c88a9b7a7b894074cbf11c215a97f07e
Ailwei <Ailweimaemu@gmail.com>
2023-06-20 08:29:10 -0700

    4. isalpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,10 +7,20 @@
  */
 int _isalpha(int c)
 {
-	if ((c = 'a' && c <= 'z') || (c = 'A' && c <= 'Z'))
-		return (1);
-	else
-		return (0);
-	       	
+	   int i;
 
+    for (i = 'a'; i <= 'z'; i++)
+    {
+        if (c == i)
+            return 1;
+    }
+
+    for (i = 'A'; i <= 'Z'; i++)
+    {
+        if (c == i)
+            return 1;
+    }
+
+    return 0;
+}
 }
