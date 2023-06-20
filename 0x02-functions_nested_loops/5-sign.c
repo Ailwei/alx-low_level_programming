9de57376e0a85a2fb901188155a4dada9d0c04a3
Ailwei <Ailweimaemu@gmail.com>
2023-06-20 09:12:38 -0700

    5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -7,18 +7,19 @@
  */
 int print_sign(int n)
 {
-	int n;
-
-	for (n = 0; n > 0; n++)
-		printf('+');
-		return (1);
-		for (n = 0; n == 0; n++)
-			printf('+');
-		return (0)
-			for (n = 0; n <= 0; n--)
-				printf('-');
-		return (-1);
-
+	if (n > 0)
+	{
+		_putchar('+');
+		return 1;
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return 0;
+	}
+	else
+	{
+		_putchar('-');
+		return -1;
+	}
 }
-
-
