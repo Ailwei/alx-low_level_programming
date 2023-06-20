d1e98dfb75d66904940f76bd40d7eaf655260613
Ailwei <Ailweimaemu@gmail.com>
2023-06-20 07:28:40 -0700

    2. 10 x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -13,9 +13,8 @@ void print_alphabet_x10(void)
 	for (i = 0; i < 10; i++)
 	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-		{
-			 _putchar(alphabet);
-		}
+		      _putchar(alphabet);
+		
 		 _putchar('\n');
 	}
 }
