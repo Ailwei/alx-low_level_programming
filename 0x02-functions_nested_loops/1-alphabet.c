92e3ebeda50a0d626a5720a3fee253acc29b1487
Ailwei <Ailweimaemu@gmail.com>
2023-06-20 05:37:03 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main - function that prints the alphabet, in lowercase,
+ * follwed by a new line
+ *
+ * Return:Always 0 success
+ */
+int main(void)
+{
+	_putchar('a');
+	_putchar('\n');
+
+	return (0);
+}
