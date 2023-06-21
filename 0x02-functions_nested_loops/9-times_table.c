eed47705d1a05a61c3c1022ca1d3baa58b0d6e01
Ailwei <Ailweimaemu@gmail.com>
2023-06-20 18:57:37 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -3,32 +3,31 @@
 /**
  * times_table - prints the 9 times table, starting with 0
  */
-void times_table(void)
-{
-	int i, j, k;
+void times_table(void) {
+    int i, j;
 
-	for (i = 0; i < 10; j++)
-	{
-		for (j = 0; j < 10; j++)
-		{
-			k = j * i;
-			if (j == 0)
-			{
-				_putchar(k + '0');
-			}
-		}	if (k < 10 && j != 0)
-			{
-			_putchar(',');
-			_putchar(' ');
-			_putchar(' ');
-			_putchar(k + '0');
-			} else if (k >= 10)
-			{
-			_putchar(',');
-			_putchar(' ');
-			_putchar((k / 10) + '0');
-			_putchar((k % 10) + '0');
-			}
-	}
-	_putchar('\n');
+    for (i = 0; i <= 9; i++) {
+        for (j = 0; j <= 9; j++) {
+            int result = i * j;
+
+            // Print tens digit
+            if (result >= 10) {
+                _putchar('0' + (result / 10));
+            } else {
+                _putchar(' ');
+            }
+
+            // Print ones digit
+            _putchar('0' + (result % 10));
+
+            // Print separator
+            _putchar(',');
+
+            // Print space
+            _putchar(' ');
+        }
+
+        // Print newline
+        _putchar('\n');
+    }
 }
