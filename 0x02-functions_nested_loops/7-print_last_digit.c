2a8727ea642de8ac5b1fe1f100cf553252c5ded6
Ailwei <Ailweimaemu@gmail.com>
2023-06-20 17:31:21 -0700

    7. There are only 3 colors, 10 digits, and 7 notes; it's what we do with them that's important

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,11 +1,17 @@
 #include "main.h"
-#include <iostream>
 /**
+ * Print_last_digit - prints the last digit of a number
+ * @n: The number to be treated
+ * Return:Value of the last digit of number
  */
 int print_last_digit(int number)
 {
-	int last_digit = number % 10;  // Use modulo operator to get the remainder (last digit)
-    std::cout << "Last digit: " << last_digit << std::endl;
-    return last_digit;
-
+	int last;
+	last = number % 10;
+	if (last < 0)
+	{
+		last = last * -1;
+	}
+	_putchar(last + '0');
+	return(last);
 }
