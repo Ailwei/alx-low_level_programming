6b94c9c071a30ece66cdbf0a246fceba530de8c6
Ailwei <Ailweimaemu@gmail.com>
2023-06-21 15:53:05 -0700

    15. Even Liber Abbaci

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,21 +7,16 @@
  */
 int main(void)
 {
-    unsigned long fib1 = 1, fib2 = 2, fib_next, count;
+       	unsigned long fib1 = 1, fib2 = 2, fib_next, count;
 
-    printf("%lu, %lu", fib1, fib2);
-
-    for (count = 3; count <= 98; count++)
-    {
-        fib_next = fib1 + fib2;
-        printf(", %lu", fib_next);
-        
-        fib1 = fib2;
-        fib2 = fib_next;
-    }
-
-    printf("\n");
-
-    return 0;
+	printf("%lu, %lu", fib1, fib2);
+	for (count = 3; count <= 98; count++)
+	{
+		fib_next = fib1 + fib2;
+		printf(", %lu", fib_next);
+		fib1 = fib2;
+		fib2 = fib_next;
+	}
+	printf("\n");
+	return (0);
 }
-
