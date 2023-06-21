b252a39df76ed5515d508c4d06d5ef36ee29ec4e
Ailwei <Ailweimaemu@gmail.com>
2023-06-21 05:09:54 -0700

    11. 98 Battery Street, the OG

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -4,9 +4,11 @@
  */
 void print_to_98(int n)
 {
-	for (n = 0; n <= 98; n++)
-		printf("%d", n);
-	if (n != 98)
+	int i;
+
+	for (i = n; i <= 98; i++)
+		printf("%d", i);
+	if (i != 98)
 	{
 		printf(", ");
         }
