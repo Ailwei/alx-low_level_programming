b35b30cd104d014cb12aa9a2800e32a37b59157d
Ailwei <Ailweimaemu@gmail.com>
2023-06-20 19:00:40 -0700

    9. Learn your times table

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -10,24 +10,16 @@ void times_table(void) {
         for (j = 0; j <= 9; j++) {
             int result = i * j;
 
-            // Print tens digit
+            
             if (result >= 10) {
                 _putchar('0' + (result / 10));
             } else {
                 _putchar(' ');
             }
-
-            // Print ones digit
             _putchar('0' + (result % 10));
-
-            // Print separator
             _putchar(',');
-
-            // Print space
             _putchar(' ');
         }
-
-        // Print newline
         _putchar('\n');
     }
 }
