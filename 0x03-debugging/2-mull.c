5d5379f78e18a594d3bb32f7c099928cd556b944
Ailwei <Ailweimaemu@gmail.com>
2023-06-22 14:06:29 -0700

    2. Collaboration is multiplication

---
+++ 0x03-debugging/2-mull.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - function that multiplies two integers
+ * @a: First value
+ * @b: second value
+ *
+ * Return:result of the multiplication of two values
+ */
+int mul(int a, int b)
+{
+	int result;
+	result = a * b;
+	return (result);
+}
