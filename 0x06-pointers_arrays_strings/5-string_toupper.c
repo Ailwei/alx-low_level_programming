412cb50652ef63fc600e06838703fc794d440ae7
Ailwei <Ailweimaemu@gmail.com>
2023-06-28 15:24:45 -0700

    5. Always look up

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * string_toupper - Changes all lowercase letters of a string to uppercase.
+ * @str: The string to convert.
+ *
+ * Return: Pointer to the resulting string.
+ */
+char *string_toupper(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        if (str[i] >= 'a' && str[i] <= 'z')
+        {
+            str[i] = str[i] - 32;  // Convert lowercase letter to uppercase
+        }
+        i++;
+    }
+
+    return str;
+}
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@ char *_strncat(char *dest,const char *src, int n);
 int _strcmp(const char *s1,const char *s2);
 void reverse_array(int *a, int n);
 void _putchar(char *s);
+char *string_toupper(char *);
 char *_strncpy(char *dest, const char *src, int n);
 
 
