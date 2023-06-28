ec4b37dfc9e03b5b8c2b2c33c04113ec547c2b1f
Ailwei <Ailweimaemu@gmail.com>
2023-06-28 15:08:55 -0700

    3. strcmp

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -3,11 +3,11 @@
 
 char *_strcat(char *dest, char *src);
 char *_strncat(char *dest,const char *src, int n);
-int _strcmp(char *s1, char *s2);
+int _strcmp(const char *s1,const char *s2);
 void reverse_array(int *a, int n);
 void _putchar(char *s);
 char *_strncpy(char *dest, const char *src, int n);
-int _strcmp(const char *s1,const char *s2);
+
 
 
 #endif
