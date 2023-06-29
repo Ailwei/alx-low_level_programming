8735c8747d645dd87e079e345d2204e2469c86a7
Ailwei <Ailweimaemu@gmail.com>
2023-06-29 14:40:57 -0700

    9. Numbers have life; they're not just symbols on paper

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -5,7 +5,7 @@ char *_strcat(char *dest, char *src);
 char *_strncat(char *dest,const char *src, int n);
 int _strcmp(const char *s1,const char *s2);
 void reverse_array(int *a, int n);
-void _putchar(char *s);
+int _putchar(char c);
 char *string_toupper(char *);
 char *_strncpy(char *dest, const char *src, int n);
 char *cap_string(char *);
