e371c1550247d80a14f01efb2663a29618b5c354
Ailwei <Ailweimaemu@gmail.com>
2023-06-30 04:25:32 -0700

    12. Noise is a buffer, more effective than cubicles or booth walls

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -17,19 +17,21 @@
  */
 void print_buffer(char *b, int size)
 {
+	int i,j;
+
 	if (size <= 0)
 	{
 		printf("\n");
 		return;
 	}
 
-	for (int i = 0; i < size; i += 10)
+	for (i = 0; i < size; i += 10)
 	{
 		/* Print the position of the first byte in the line */
 		printf("%08x  ", i);
 
 		/* Print the hexadecimal content of the buffer */
-		for (int j = i; j < i + 10; j += 2)
+		for (j = i; j < i + 10; j += 2)
 		{
 			if (j < size)
 			{
@@ -50,7 +52,7 @@ void print_buffer(char *b, int size)
 		}
 
 		/* Print the content of the buffer */
-		for (int j = i; j < i + 10 && j < size; j++)
+		for (j = i; j < i + 10 && j < size; j++)
 		{
 			if (isprint(b[j]))
 			{
