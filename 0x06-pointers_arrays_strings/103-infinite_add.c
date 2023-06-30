047d65d6756f3498f774a78c3244c7f2bc4348f0
Ailwei <Ailweimaemu@gmail.com>
2023-06-30 06:06:33 -0700

    11. It is the addition of strangeness to beauty that constitutes the romantic character in art

--- 0x06-pointers_arrays_strings/103-infinite_add.c
+++ 0x06-pointers_arrays_strings/103-infinite_add.c
@@ -9,10 +9,10 @@
  * @r: Buffer to store the result.
  * @size_r: Size of the result buffer.
  *
- * The function adds the two numbers represented as strings, n1 and n2, and stores the result in the buffer r.
+ * The func adds two nums represent as strings, n1& n2, and store the buffer r.
  * It returns a pointer to the result.
  * If the result cannot be stored in r, the function returns 0.
- * The function assumes that n1 and n2 are positive numbers or 0, and that they only contain digits.
+ * The func assume that n1& n2 are + numbers or 0,and that they contain digits.
  * n1 and n2 will never be empty.
  */
 #include <stdio.h>
@@ -23,20 +23,20 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 {
 	int len1 = strlen(n1);
 	int len2 = strlen(n2);
-	int max_len = size_r - 1;  /* Leave space for null terminator */
+	int max_len = size_r - 1;/* Leave space for null terminator */
 	int carry = 0;
 	int i, j, k;
 
 	if (len1 + len2 > max_len)
 	{
-		return NULL;  /* Result cannot be stored in r*/
+		return (NULL);/* Result cannot be stored in r*/
 	}
 
 	i = len1 - 1;
 	j = len2 - 1;
 	k = max_len - 1;
 
-	r[max_len] = '\0';  /* Null terminator*/
+	r[max_len] = '\0';/* Null terminator*/
 
 	while (i >= 0 || j >= 0 || carry != 0)
 	{
@@ -59,5 +59,5 @@ char *infinite_add(char *n1, char *n2, char *r, int size_r)
 		k--;
 	}
 
-	return &r[k + 1];
+	return (&r[k + 1]);
 }
