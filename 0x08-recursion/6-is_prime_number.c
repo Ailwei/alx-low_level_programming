909781e8abb909ebc4f9495e4b3543fe6330f864
Ailwei <Ailweimaemu@gmail.com>
2023-07-05 15:23:39 -0700

    6. Inception. Is it possible?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -5,19 +5,19 @@
  * is_prime_number - Checks if a number is prime.
  * @n: The number to check.
  *
- * Description: This function checks whether a given number 'n' is prime or not.
+ * Description: This function checks whether
+ * a given number 'n' is prime or not.
  *              It returns 1 if the number is prime, and 0 otherwise.
  *
  * Return: 1 if 'n' is prime, 0 otherwise.
  */
 int is_prime_number(int n)
 {
-    /* 0 and 1 are not prime numbers */
-    if (n <= 1)
-        return 0;
-
-    /* Check for divisibility by numbers up to sqrt(n) */
-    return is_prime_helper(n, 2);
+	/* 0 and 1 are not prime numbers */
+	if (n <= 1)
+		return (0);
+	/* Check for divisibility by numbers up to sqrt(n) */
+	return (is_prime_helper(n, 2));
 }
 
 /**
@@ -29,15 +29,12 @@ int is_prime_number(int n)
  */
 int is_prime_helper(int n, int divisor)
 {
-    /* Base case: n is divisible by the divisor */
-    if (n % divisor == 0)
-        return 0;
-
-    /* Base case: All possible divisors have been checked */
-    if (divisor * divisor > n)
-        return 1;
-
-    /* Recursive case: Check the next divisor */
-    return is_prime_helper(n, divisor + 1);
+	/* Base case: n is divisible by the divisor */
+	if (n % divisor == 0)
+		return (0);
+	/* Base case: All possible divisors have been checked */
+	if (divisor * divisor > n)
+		return (1);
+	/* Recursive case: Check the next divisor */
+	return (is_prime_helper(n, divisor + 1));
 }
-
