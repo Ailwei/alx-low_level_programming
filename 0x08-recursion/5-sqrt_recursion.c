fb6c6f712122505509c64ac7b846258ba3a0f99a
Ailwei <Ailweimaemu@gmail.com>
2023-07-05 06:42:58 -0700

    5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -24,3 +24,30 @@ int _sqrt_recursion(int n)
 
 	return _sqrt_helper(n, 1, n);
 }
+/**
+ * _sqrt_helper - Helper function to find the square root using binary search.
+ * @n: The number to calculate the square root of.
+ * @start: The starting value of the search range.
+ * @end: The ending value of the search range.
+ *
+ * Return: The square root of 'n', or -1 if 'n' does not have a natural square root.
+ */
+int _sqrt_helper(int n, int start, int end)
+{
+	int mid = (start + end) / 2;
+
+	if (start <= end)
+	{
+		/* Check if the middle value is the square root */
+
+		if (mid * mid == n)
+			return mid;
+		/* If the middle value is greater, search in the lower half */
+		else if (mid * mid > n)
+			return _sqrt_helper(n, start, mid - 1);
+		/* If the middle value is smaller, search in the upper half */
+		return _sqrt_helper(n, mid + 1, end);
+	}
+	/* Error case: 'n' does not have a natural square root */
+	return -1;
+}
