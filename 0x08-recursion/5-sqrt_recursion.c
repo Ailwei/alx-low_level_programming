3e0b7fff30ed9e8e9c45f74c7331d6eb6efd9d02
Ailwei <Ailweimaemu@gmail.com>
2023-07-05 15:35:00 -0700

    5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,50 +1,39 @@
 #include "main.h"
 #include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
 /**
  * _sqrt_recursion - Calculates the natural square root of a number.
  * @n: The number to calculate the square root of.
  *
- * Description: This function calculates the natural square root of
- *              a number 'n' using a recursive approach. If 'n' does not
- *              have a natural square root, it returns -1.
- *
- * Return: The natural square root of 'n', or -1 if 'n' does not have
- *         a natural square root.
+ * Return: The square root of @n, or -1 if no natural square root exists.
  */
 int _sqrt_recursion(int n)
 {
-	if (n < 0)
-		return (-1);
-	else if (n == 0 || n == 1)
-		return (n);
-	else
-		return (_sqrt_helper(n, 1, n));
+	return (_sqrt_recursive_helper(n, 0, n));
 }
 
 /**
- * sqrt_helper - Helper function to find the square root using binary search.
+ * _sqrt_recursive_helper - Helper function for calculating the square root recursively.
  * @n: The number to calculate the square root of.
- * @start: The starting value of the search range.
- * @end: The ending value of the search range.
+ * @start: The starting point of the range to search for the square root.
+ * @end: The ending point of the range to search for the square root.
  *
- * Description: This function performs a binary search to find the square root
- *              of a number within a given range. If the square root is found,
- *              it returns the value. Otherwise, it returns -1.
+ * Return: The square root of @n within the given range, or -1 if not found.
  */
-int _sqrt_helper(int n, int start, int end)
+int _sqrt_recursive_helper(int n, int start, int end)
 {
-	int mid, square;
-
 	if (start > end)
 		return (-1);
 
+	int mid = start + (end - start) / 2;
+	int square = mid * mid;
+
 	if (square == n)
 		return (mid);
-	else if (square > n)
-		return (_sqrt_helper(n, start, mid - 1));
+	else if (square < n)
+		return (_sqrt_recursive_helper(n, mid + 1, end));
 	else
-		return (_sqrt_helper(n, mid + 1, end));
+		return (_sqrt_recursive_helper(n, start, mid - 1));
 }
-
