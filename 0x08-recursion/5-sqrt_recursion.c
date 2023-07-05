6275f8bc2f4afb56844525ccbf0eaa6146892adc
Ailwei <Ailweimaemu@gmail.com>
2023-07-05 14:33:47 -0700

    5. Your subconscious is looking for the dreamer

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -23,10 +23,10 @@ int _sqrt_recursion(int n)
 	/* Base case: square root of 0 or 1 is the number itself */
 
 	if (n == 0 || n == 1)
-		return n;
+		return (n);
 	/* Call a helper function with additional parameters */
 
-	return _sqrt_helper(n, 1, n);
+	return (_sqrt_helper(n, 1, n));
 }
 /**
  * _sqrt_helper - Helper function to find the square root using binary search.
@@ -34,24 +34,25 @@ int _sqrt_recursion(int n)
  * @start: The starting value of the search range.
  * @end: The ending value of the search range.
  *
- * Return: The square root of 'n', or -1 if 'n' does not have a natural square root.
+ * Return: The square root of 'n', or -1 if 'n'
+ * does not have a natural square root.
  */
 int _sqrt_helper(int n, int start, int end)
 {
-	int mid = (start + end) / 2;
+	int mid = (start - end) / 2;
 
-	if (start <= end)
+	if (start > end)
 	{
 		/* Check if the middle value is the square root */
 
 		if (mid * mid == n)
-			return mid;
+			return (mid);
 		/* If the middle value is greater, search in the lower half */
 		else if (mid * mid > n)
-			return _sqrt_helper(n, start, mid - 1);
+			return (_sqrt_helper(n, start, mid - 1));
 		/* If the middle value is smaller, search in the upper half */
-		return _sqrt_helper(n, mid + 1, end);
+		return (_sqrt_helper(n, mid - 1, end));
 	}
 	/* Error case: 'n' does not have a natural square root */
-	return -1;
+	return (-1);
 }
