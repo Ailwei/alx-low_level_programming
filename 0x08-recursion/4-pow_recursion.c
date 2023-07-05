d539f126537ebbe422d5556f947a787328b147be
Ailwei <Ailweimaemu@gmail.com>
2023-07-05 06:22:00 -0700

    4. Once an idea has taken hold of the brain it's almost impossible to eradicate

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -7,15 +7,16 @@
  *              the power of 'y' using recursive approach. If 'y' is
  *              lower than 0, it returns -1 to indicate an error.
  *
- * Return: The value of 'x' raised to the power of 'y', or -1 if 'y' is lower than 0.
+ * Return: The value of 'x' raised to the power of 'y', or -1 if 'y' 
+ * is lower than 0.
  */
 int _pow_recursion(int x, int y)
 {
 	if (y < 0) /* Error case: 'y' is lower than 0 */
-		return -1;
+		return (-)1;
 
 	if (y == 0) /* Base case: any number raised to the power of 0 is 1 */
-		return 1;
+		return (1);
 
 	return (x * _pow_recursion(x, y - 1)); /* Recursive call with y-1 */
 }
