c49a761c95209308b8bc16b0061c586709163e49
Ailwei <Ailweimaemu@gmail.com>
2023-07-06 13:55:22 -0700

    8. Inception. Now, before you bother telling me it's impossible...

--- 0x08-recursion/101-wildcmp.c
+++ 0x08-recursion/101-wildcmp.c
@@ -11,33 +11,30 @@
  */
 int wildcmp(char *s1, char *s2)
 {
-    if (*s1 == '\0' && *s2 == '\0')
-    {
+	if (*s1 == '\0' && *s2 == '\0')
+	{
         /* Base case: both strings are empty, they are considered identical */
-        return 1;
-    }
-    
-    if (*s1 == *s2 || *s2 == '*')
-    {
-        if (*s2 == '*' && *(s2 + 1) == '\0')
-        {
-            /* If s2 has a single wildcard '*', it matches the remaining characters in s1 */
-            return 1;
-        }
-        
-        if (*s1 != '\0')
-        {
-            if (wildcmp(s1 + 1, s2 + 1) == 1)
-                return 1;
-        }
-        
-        if (*s2 == '*')
-        {
-            if (wildcmp(s1, s2 + 1) == 1)
-                return 1;
-        }
-    }
-    
-    return 0; /* Characters don't match and string doesn't have a wildcard */
+		return (1);
+	}
+	if (*s1 == *s2 || *s2 == '*')
+	{
+		if (*s2 == '*' && *(s2 + 1) == '\0')
+		{
+			/** If s2 has a single wildcard '*', 
+			 * it matches the remaining characters in s1 
+			 */
+			return (1);
+		}
+		if (*s1 != '\0')
+		{
+			if (wildcmp(s1 + 1, s2 + 1) == 1)
+				return (1);
+		}
+		if (*s2 == '*')
+		{
+			if (wildcmp(s1, s2 + 1) == 1)
+				return (1);
+		}
+	}
+	return (0); /* Characters don't match and string doesn't have a wildcard */
 }
-
