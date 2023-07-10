c2374e6cb2a8e7b4bc5036af6694621a4ac55518
Ailwei <Ailweimaemu@gmail.com>
2023-07-10 15:32:37 -0700

    5. It isn't the mountains ahead to climb that wear you out; it's the pebble in your shoe

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -1,25 +1,37 @@
-#include <stdio.h>
 #include "main.h"
 #include <stdlib.h>
 #include <string.h>
 
-/**
- * main - Entry point of the program.
- * @argc: The argument count.
- * @argv: The argument vector.
- *
- * Return: 0 on success, 1 on failure.
- */
-int main(int argc, char *argv[])
-{
-	char *concatenated_args = argstostr(argc - 1, argv + 1);
+char *argstostr(int ac, char **av) {
+    int total_length = 0;
+    int position = 0;
+    int i, j;
+    char *result;
 
-	if (concatenated_args == NULL)
-	{
-		printf("Failed to concatenate arguments.\n");
-		return (1);
-	}
-	printf("Concatenated arguments:\n%s\n", concatenated_args);
-	free(concatenated_args);  /* Remember to free the allocated memory */
-	return (0);
+    if (ac == 0 || av == NULL) {
+        return NULL;
+    }
+
+    /* Calculate the total length of the concatenated string */
+    for (i = 0; i < ac; i++) {
+        total_length += strlen(av[i]) + 1;  /* +1 for newline character */
+    }
+
+    /* Allocate memory for the concatenated string */
+    result = (char *)malloc((total_length + 1) * sizeof(char));
+    if (result == NULL) {
+        return NULL;  /* Failed to allocate memory */
+    }
+
+    /* Concatenate the arguments with newlines */
+    for (i = 0; i < ac; i++) {
+        for (j = 0; av[i][j] != '\0'; j++) {
+            result[position++] = av[i][j];
+        }
+        result[position++] = '\n';
+    }
+
+    result[total_length] = '\0';  /* Replace the last newline with null terminator */
+
+    return result;
 }
