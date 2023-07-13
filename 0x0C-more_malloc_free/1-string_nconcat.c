3a631e81b28a5f4654df65fe8f51776aaee006be
Ailwei <Ailweimaemu@gmail.com>
2023-07-13 03:33:54 -0700

    1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,22 +15,19 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
 	unsigned int len1 = 0, len2 = 0;
 	char *concatenated;
-	
+
 	/* Treat NULL strings as empty strings */
 	if (s1 == NULL)
 		s1 = "";
 	if (s2 == NULL)
 		s2 = "";
-	
+
 	/* Calculate the lengths of s1 and s2 */
 	len1 = strlen(s1);
 	len2 = strlen(s2);
-	
 	/* If n is greater or equal to the length of s2, use the entire string */
-	
 	if (n >= len2)
 		n = len2;
-	
 	/* Allocate memory for the concatenated string */
 	concatenated = malloc((len1 + n + 1) * sizeof(char));
 	if (concatenated == NULL)
@@ -40,7 +37,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	/* Copy s1 into the concatenated string */
 	strcpy(concatenated, s1);
-	
 	/* Append the first n bytes of s2 to the concatenated string */
 	strncat(concatenated, s2, n);
 	return (concatenated);
