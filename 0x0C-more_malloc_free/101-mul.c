6c441bd3ada355460deacff09741f0d64a5af131
Ailwei <Ailweimaemu@gmail.com>
2023-07-13 04:34:54 -0700

    5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -68,35 +68,5 @@ void multiply(char *num1, char *num2)
     printf("\n");
 
     /* Free allocated memory */
-    free(result);
+     free(result);
 }
-
-/**
- * main - Entry point of the program
- * @argc: The number of command-line arguments
- * @argv: An array of strings representing the command-line arguments
- *
- * Return: 0 on success, 98 on error
- */
-int main(int argc, char *argv[])
-{
-    if (argc != 3)
-    {
-        printf("Error\n");
-        return 98;
-    }
-
-    char *num1 = argv[1];
-    char *num2 = argv[2];
-
-    if (!is_valid_number(num1) || !is_valid_number(num2))
-    {
-        printf("Error\n");
-        return 98;
-    }
-
-    multiply(num1, num2);
-
-    return 0;
-}
-
