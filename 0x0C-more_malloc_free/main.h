337a5c747e98d649304e08de3f3909eba0405755
Ailwei <Ailweimaemu@gmail.com>
2023-07-13 13:22:12 -0700

    5. We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -4,5 +4,7 @@ void *malloc_checked(unsigned int b);
 int is_valid_number(char *str);
 void multiply(char *num1, char *num2);
 int _putchar(char c);
+int multiply(const char *num1, const char *num2);
 
+void validate_arguments(int argc, char *argv[]);
 #endif
