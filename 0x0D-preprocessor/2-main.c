7338ce9ed058fc1771ef977e5df0ef0a8c9c2d05
Ailwei <Ailweimaemu@gmail.com>
2023-07-17 03:33:46 -0700

    2. File name

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,5 +1,16 @@
 #include <stdio.h>
 
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This function serves as the entry point of the program.
+ * It prints a welcome message and returns 0 to indicate successful execution.
+ *
+ * Return: Always 0 (success)
+ */
+
 int main(void)
 {
 	printf("Compiled from file: %s\n", __FILE__);
