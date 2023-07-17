d779c407d00956c96deea846cc761688fea7d94b
Ailwei <Ailweimaemu@gmail.com>
2023-07-17 06:20:34 -0700

    5. How many legs does a dog have if you call his tail a leg? Four. Saying that a tail is a leg doesn't make it a leg

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * free_dog - Frees the memory allocated for a dog structure
+ * @d: Pointer to the dog structure to free
+ *
+ * Description: This function frees the memory allocated for the dog
+ *              structure pointed to by @d, including the memory
+ *              allocated for the name and owner strings.
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+		return;
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
