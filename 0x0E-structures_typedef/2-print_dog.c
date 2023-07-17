e4f3148ead3a913e29f8c595157ce2fb38d9045e
Ailwei <Ailweimaemu@gmail.com>
2023-07-17 05:48:42 -0700

    2. A dog will teach you unconditional love. If you can have that in your life, things won't be too bad

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "dog.h"
+
+/**
+ * print_dog - Prints the details of a struct dog
+ * @d: Pointer to the struct dog to print
+ *
+ * Description: This function prints the contents of the struct dog
+ *              pointed to by @d. If any element of @d is NULL,
+ *              "(nil)" is printed instead of that element.
+ *              If @d is NULL, nothing is printed.
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+	printf("Name: %s\n", (d->name != NULL) ? d->name : "(nil)");
+	printf("Age: %.1f\n", d->age);
+	printf("Owner: %s\n", (d->owner != NULL) ? d->owner : "(nil)");
+}
