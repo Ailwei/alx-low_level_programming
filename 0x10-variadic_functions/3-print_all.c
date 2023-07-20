ec29442be1b4a22c4d54bee77e8b08c12a3f4860
Ailwei <Ailweimaemu@gmail.com>
2023-07-20 15:34:39 -0700

    3. To be is a to be the value of a variable

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -10,47 +10,52 @@
  * Print a new line at the end of the function.
  */
 void print_all(const char * const format, ...)
+/**
+ * print_all - prints anything.
+ * @format: a list of types of arguments passed to the function.
+ *
+ * Return: no return.
+ */
+void print_all(const char * const format, ...)
 {
-	va_list args;
-	const char *fmt_ptr = format;
-	char c_val;
-	int i_val;
-	float f_val;
-	char *s_val;
+	va_list valist;
+	unsigned int i = 0, j, c = 0;
+	char *str;
+	const char t_arg[] = "cifs";
 
-	va_start(args, format);
-
-	while (format && *fmt_ptr)
+	va_start(valist, format);
+	while (format && format[i])
 	{
-		if (*fmt_ptr == 'c')
+		j = 0;
+		while (t_arg[j])
 		{
-			c_val = va_arg(args, int);
-			printf("%c", c_val);
+			if (format[i] == t_arg[j] && c)
+			{
+				printf(", ");
+				break;
+			} j++;
 		}
-		else if (*fmt_ptr == 'i')
+		switch (format[i])
 		{
-			i_val = va_arg(args, int);
-			printf("%d", i_val);
-		}
-		else if (*fmt_ptr == 'f')
-		{
-			f_val = va_arg(args, double);
-			printf("%f", f_val);
-		}
-		else if (*fmt_ptr == 's')
-		{
-			s_val = va_arg(args, char *);
-			if (s_val == NULL)
-				printf("(nil)");
-			else
-				printf("%s", s_val);
-		}
-		fmt_ptr++;
-
-		if (*fmt_ptr && (*fmt_ptr == 'c' || *fmt_ptr == 'i' || *fmt_ptr == 'f' || *fmt_ptr == 's'))
-			printf(", ");
+			case 'c':
+				printf("%c", va_arg(valist, int)), c = 1;
+				break;
+			case 'i':
+				printf("%d", va_arg(valist, int)), c = 1;
+				break;
+			case 'f':
+				printf("%f", va_arg(valist, double)), c = 1;
+				break;
+			case 's':
+				str = va_arg(valist, char *), c = 1;
+				if (!str)
+				{
+					printf("(nil)");
+					break;
+				}
+				printf("%s", str);
+				break;
+		} i++;
 	}
-
-	va_end(args);
-	printf("\n");
+	printf("\n"), va_end(valist);
 }
