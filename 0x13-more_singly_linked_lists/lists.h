6ebfbb65543ab94437ca8afc21537b525c6caf48
Ailwei <Ailweimaemu@gmail.com>
2023-07-31 14:18:10 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -3,11 +3,19 @@
 
 #include <stddef.h>
 
+/**
+ * struct listint_s - A singly linked list node.
+ * @n: Integer data for the node.
+ * @next: Pointer to the next node in the list.
+ *
+ * Description: A structure to represent a node in a singly linked list.
+ */
+
 /* Define the listint_t structure representing a linked list node */
 typedef struct listint_s
 {
-    int n;
-    struct listint_s *next;
+	int n;
+	struct listint_s *next;
 } listint_t;
 
 /* Function prototype to print all elements of a listint_t list */
