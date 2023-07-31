fcd391b938e8c4565744151cb21be006c2774c13
Ailwei <Ailweimaemu@gmail.com>
2023-07-31 14:12:08 -0700

    0. Print list

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,6 +1,16 @@
 #ifndef LISTS_H
 #define LISTS_H
 
+#include <stddef.h>
+
+/* Define the listint_t structure representing a linked list node */
+typedef struct listint_s
+{
+    int n;
+    struct listint_s *next;
+} listint_t;
+
+/* Function prototype to print all elements of a listint_t list */
 size_t print_listint(const listint_t *h);
 
 #endif
