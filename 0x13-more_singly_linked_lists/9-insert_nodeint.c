cb51ac594cb3fd4326824d616c0e2ea514fbc185
Ailwei <Ailweimaemu@gmail.com>
2023-08-01 11:06:26 -0700

    9. Insert

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,20 @@
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * free_listint - frees a listint_t linked list
+ * @head: pointer to the head of the list
+ */
+void free_listint(struct listint_s *head)
+{
+	struct listint_s *current = head;
+	struct listint_s *next_node;
+
+	while (current != NULL)
+	{
+		next_node = current->next;
+		free(current);
+		current = next_node;
+	}
+}
