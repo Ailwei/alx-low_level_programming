ee0f5a7efd79f37ac9bf8bd032f9b40ccf8ecda6
Ailwei <Ailweimaemu@gmail.com>
2023-08-02 05:43:56 -0700

    10 delete

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -2,30 +2,29 @@
 #include <stdlib.h>
 
 /**
- * delete_nodeint_at_index - deletes a node at a given position
- * @head: pointer to the beginning of linked list
- * @index: index to be deleted
- * Return: 1 if successful, -1 if fails
+ * delete_nodeint_at_index - deletes the node at index index of a linked list
+ * @head: pointer to the beginning of the linked list
+ * @index: index of the node to be deleted (starting from 0)
+ * Return: 1 if succeeded, -1 if failed
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return (-1);
 
-	/* Special case: delete the first node */
+	listint_t *current = *head;
+	listint_t *prev = NULL;
+	unsigned int i = 0;
+
+	/* Handle special case: delete the first node */
 	if (index == 0)
 	{
-		listint_t *temp = *head;
 		*head = (*head)->next;
-		free(temp);
+		free(current);
 		return (1);
 	}
 
-	listint_t *prev = NULL;
-	listint_t *current = *head;
-	unsigned int i = 0;
-
-	/* Traverse the list to find the node at the specified index */
+	/* Traverse to find the node at the specified index or the end of the list */
 	while (current != NULL && i < index)
 	{
 		prev = current;
@@ -33,10 +32,11 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		i++;
 	}
 
+	/* If index is out of bounds, return -1 (failed) */
 	if (current == NULL)
-		return (-1); /* Index is out of bounds */
+		return (-1);
 
-	/* Perform the deletion by updating the pointers */
+	/* Perform the deletion by updating the pointers and freeing the node */
 	prev->next = current->next;
 	free(current);
 	return (1);
