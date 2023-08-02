a00b47d5a25c768e4408150879274b4f15b503d2
Ailwei <Ailweimaemu@gmail.com>
2023-08-01 23:36:06 -0700

    10 delete nodeint

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -3,55 +3,39 @@
 #include "lists.h"
 
 /**
- * struct listint_s - singly linked list
- * @data: integer
- * @next: points to the next node
+ * delete_nodeint_at_index - Deletes the node at the given index of a linked list.
+ * @head: A pointer to the head of the linked list.
+ * @index: The index of the node to be deleted.
  *
- * Description: singly linked list node structure
- */
-typedef struct listint_s
-{
-    int data;
-    struct listint_s *next;
-} listint_t;
-
-/**
- * delete_nodeint_at_index - Deletes the node at index index of a linked list.
- * @head: Pointer to a pointer to the head of the linked list.
- * @index: Index of the node that should be deleted (starts at 0).
- *
- * Return: 1 if successful, -1 if failed.
+ * Return: 1 if the deletion is successful, -1 if it fails.
  */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
-    if (*head == NULL)
-        return (-1);
+	unsigned int i;
 
-    if (index == 0)
-    {
-        listint_t *temp = *head;
-        *head = (*head)->next;
-        free(temp);
-        return (1);
-    }
+	if (*head == NULL || index < 0)
+		return (-1);
 
-    listint_t *current = *head;
-    listint_t *previous = NULL;
-    unsigned int count = 0;
+	listint_t *current = *head;
+	listint_t *temp;
 
-    while (current != NULL)
-    {
-        if (count == index)
-        {
-            previous->next = current->next;
-            free(current);
-            return (1);
-        }
-        previous = current;
-        current = current->next;
-        count++;
-    }
+	if (index == 0)
+	{
+		*head = current->next;
+		free(current);
+		return (1);
+	}
 
-    return (-1);
+	for (i = 0; current != NULL && i < index - 1; i++)
+	{
+		current = current->next;
+	}
+
+	if (current == NULL || current->next == NULL)
+		return (-1);
+
+	temp = current->next;
+	current->next = temp->next;
+	free(temp);
+	return (1);
 }
-
