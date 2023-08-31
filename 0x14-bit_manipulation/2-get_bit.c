4e7898ab171369d39338fa04e7c880bbc6e80d90
Ailwei <Ailweimaemu@gmail.com>
2023-08-31 14:04:07 -0700

    2.1

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -2,7 +2,7 @@
 #include <stdio.h>
 
 /**
- * get_bit - Get the value of a bit at a given index.
+ * get_bit - Get the value of  bit at a given index.
  * @n: The unsigned long integer to extract the bit from.
  * @index: The index of the bit to retrieve (starting from 0).
  *
