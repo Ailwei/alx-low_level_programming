8c4f3e166f7bb3f993bc17c1f94bd5c1b22a7903
Ailwei <Ailweimaemu@gmail.com>
2023-09-04 04:28:10 -0700

    3. cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,156 +1,68 @@
-#include <fcntl.h>
-#include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include "main.h"
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
 
-/**
- * error_exit - Print an error message and exit.
- * @code: The exit code.
- * @message: The error message.
- */
-void error_exit(int code, const char *message)
-{
-	dprintf(STDERR_FILENO, "%s\n", message);
-	exit(code);
+#define BUFFER_SIZE 1024
+
+void print_error(const char *message) {
+    dprintf(STDERR_FILENO, "%s\n", message);
 }
 
-/**
- * read_textfile - Reads and prints a text file to the POSIX standard output.
- * @filename: The name of the file to read.
- * @letters: The number of letters it should read and print.
- *
- * Return: The actual number of letters it could read and print.
- *         0 on error or if the file can't be opened/read.
- */
-ssize_t read_textfile(const char *filename, size_t letters)
-{
-	int fd;
-	ssize_t n_read, n_written;
-	char *buf;
+int main(int argc, char *argv[]) {
+    if (argc != 3) {
+        print_error("Usage: cp file_from file_to");
+        return (97);
+    }
 
-	if (filename == NULL)
-		return (0);
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
-	buf = malloc(sizeof(char) * letters);
-	if (buf == NULL)
-	{
-		close(fd);
-		return (0);
-	}
-	n_read = read(fd, buf, letters);
-	if (n_read == -1)
-	{
-		free(buf);
-		close(fd);
-		return (0);
-	}
-	n_written = write(STDOUT_FILENO, buf, n_read);
-	free(buf);
-	close(fd);
+    const char *file_from = argv[1];
+    const char *file_to = argv[2];
 
-	if (n_written < n_read)
-		return (0);
-	return (n_written);
+    int fd_from = open(file_from, O_RDONLY);
+    if (fd_from == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+        return (98);
+    }
+
+    int fd_to = open(file_to, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH);
+    if (fd_to == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+        return (99);
+    }
+
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
+
+    while ((bytes_read = read(fd_from, buffer, BUFFER_SIZE)) > 0) {
+        if (write(fd_to, buffer, bytes_read) != bytes_read) {
+            dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file_to);
+            close(fd_from);
+            close(fd_to);
+            return (99);
+        }
+    }
+
+    if (bytes_read == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from);
+        close(fd_from);
+        close(fd_to);
+        return (98);
+    }
+
+    if (close(fd_from) == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_from);
+        return (100);
+    }
+
+    if (close(fd_to) == -1) {
+        dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd_to);
+        return (100);
+    }
+
+    return (0);
 }
 
-/**
- * create_file - Creates a file with specified text content.
- * @filename: The name of the file to create.
- * @text_content: The text content to write to the file.
- *
- * Return: 1 on success, -1 on failure.
- */
-int create_file(const char *filename, char *text_content)
-{
-	int fd, len = 0, n_written;
-
-	if (filename == NULL)
-		return (-1);
-	fd = open(filename, O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
-		return (-1);
-	if (text_content != NULL)
-	{
-		while (text_content[len])
-			len++;
-		n_written = write(fd, text_content, len);
-		if (n_written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
-	close(fd);
-	return (1);
-}
-
-/**
- * append_text_to_file - Appends text to the end of a file.
- * @filename: The name of the file.
- * @text_content: The text content to append.
- *
- * Return: 1 on success, -1 on failure.
- */
-int append_text_to_file(const char *filename, char *text_content)
-{
-	int fd, len = 0, n_written;
-
-	if (filename == NULL)
-		return (-1);
-	fd = open(filename, O_WRONLY | O_APPEND);
-	if (fd == -1)
-		return (-1);
-	if (text_content != NULL)
-	{
-		while (text_content[len])
-			len++;
-		n_written = write(fd, text_content, len);
-		if (n_written == -1)
-		{
-			close(fd);
-			return (-1);
-		}
-	}
-	close(fd);
-	return (1);
-}
-
-/**
- * main - Copies the content of one file to another.
- * @argc: The number of arguments.
- * @argv: The arguments.
- *
- * Return: 0 on success, otherwise an error code.
- */
-int main(int argc, char **argv)
-{
-	int from_fd, to_fd;
-	ssize_t n_read, n_written;
-	char buffer[1024];
-
-	if (argc != 3)
-		error_exit(97, "Usage: cp file_from file_to");
-	from_fd = open(argv[1], O_RDONLY);
-	if (from_fd == -1)
-		error_exit(98, "Error: Can't read from file NAME_OF_THE_FILE");
-	to_fd = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
-	if (to_fd == -1)
-		error_exit(99, "Error: Can't write to NAME_OF_THE_FILE");
-	while ((n_read = read(from_fd, buffer, sizeof(buffer))) > 0)
-	{
-		n_written = write(to_fd, buffer, n_read);
-		if (n_written == -1)
-			error_exit(99, "Error: Can't write to NAME_OF_THE_FILE");
-	}
-	if (n_read == -1)
-		error_exit(98, "Error: Can't read from file NAME_OF_THE_FILE");
-	if (close(from_fd) == -1)
-		error_exit(100, "Error: Can't close fd FROM_FD_VALUE");
-	if (close(to_fd) == -1)
-		error_exit(100, "Error: Can't close fd TO_FD_VALUE");
-	return (0);
-}
 
