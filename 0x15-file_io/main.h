27dad2b54bdcb15f84a6d522b2967d02363f253e
Ailwei <Ailweimaemu@gmail.com>
2023-09-04 16:42:37 -0700

    4.elf

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -14,7 +14,6 @@ int create_file(const char *filename, char *text_content);
 ssize_t read_textfile(const char *filename, size_t letters);
 int _putchar(char c);
 int append_text_to_file(const char *filename, char *text_content);
-void printElfHeader(const ElfHeader *header);
 /* Function prototypes*/
 
 void exitWithError(const char *message);
