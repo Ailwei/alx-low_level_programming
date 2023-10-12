e98226f574bd7952b845ba399f7adc8cc2f89314
Ailwei <Ailweimaemu@gmail.com>
2023-10-12 14:44:36 -0700

    11. crackme5

--- 0x17-doubly_linked_lists/103-keygen.c
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -11,24 +11,23 @@
  */
 int findLargest(char *username, int length)
 {
-    int largestChar;
-    int index;
-    unsigned int randomNum;
+	int largestChar;
+	int index;
+	unsigned int randomNum;
 
-    largestChar = *username;
-    index = 0;
+	largestChar = *username;
+	index = 0;
 
-    while (index < length)
-    {
-        if (largestChar < username[index])
-            largestChar = username[index];
-        index += 1;
-    }
+	while (index < length)
+	{
+		if (largestChar < username[index])
+			largestChar = username[index];
+		index += 1;
+	}
+	srand(largestChar ^ 14);
+	randomNum = rand();
 
-    srand(largestChar ^ 14);
-    randomNum = rand();
-
-    return (randomNum & 63);
+	return (randomNum & 63);
 }
 
 /**
@@ -40,18 +39,17 @@ int findLargest(char *username, int length)
  */
 int multiplyChars(char *username, int length)
 {
-    int result;
-    int index;
+	int result;
+	int index;
 
-    result = index = 0;
+	result = index = 0;
 
-    while (index < length)
-    {
-        result = result + username[index] * username[index];
-        index += 1;
-    }
-
-    return (((unsigned int)result ^ 239) & 63);
+	while (index < length)
+	{
+		result = result + username[index] * username[index];
+		index += 1;
+	}
+	return (((unsigned int)result ^ 239) & 63);
 }
 
 /**
@@ -62,18 +60,17 @@ int multiplyChars(char *username, int length)
  */
 int generateRandomChar(char *username)
 {
-    int randomChar;
-    int index;
+	int randomChar;
+	int index;
 
-    randomChar = index = 0;
+	randomChar = index = 0;
 
-    while (index < *username)
-    {
-        randomChar = rand();
-        index += 1;
-    }
-
-    return (((unsigned int)randomChar ^ 229) & 63);
+	while (index < *username)
+	{
+		randomChar = rand();
+		index += 1;
+	}
+	return (((unsigned int)randomChar ^ 229) & 63);
 }
 
 /**
@@ -85,54 +82,60 @@ int generateRandomChar(char *username)
  */
 int main(int argc, char **argv)
 {
-    char key[7];
-    int usernameLength, ch, index;
-    long alphabet[] = {
-        0x3877445248432d41, 0x42394530534e6c37, 0x4d6e706762695432,
-        0x74767a5835737956, 0x2b554c59634a474f, 0x71786636576a6d34,
-        0x723161513346655a, 0x6b756f494b646850
-    };
-    (void) argc;
+	char key[7];
+	int usernameLength, ch, index;
+	long alphabet[] =
+	{
+		0x3877445248432d41, 0x42394530534e6c37, 0x4d6e706762695432,
+		0x74767a5835737956, 0x2b554c59634a474f, 0x71786636576a6d34,
+		0x723161513346655a, 0x6b756f494b646850
+	};
+	(void) argc;
 
-    for (usernameLength = 0; argv[1][usernameLength]; usernameLength++)
-        ;
+	for (usernameLength = 0; argv[1][usernameLength]; usernameLength++);
 
-    /* ----------- Function 1 ----------- */
-    key[0] = ((char *)alphabet)[(usernameLength ^ 59) & 63];
+	/* ----------- Function 1 ----------- */
 
-    /* ----------- Function 2 ----------- */
-    ch = index = 0;
-    while (index < usernameLength)
-    {
-        ch = ch + argv[1][index];
-        index = index + 1;
-    }
-    key[1] = ((char *)alphabet)[(ch ^ 79) & 63];
+	key[0] = ((char *)alphabet)[(usernameLength ^ 59) & 63];
 
-    /* ----------- Function 3 ----------- */
-    ch = 1;
-    index = 0;
-    while (index < usernameLength)
-    {
-        ch = argv[1][index] * ch;
-        index = index + 1;
-    }
-    key[2] = ((char *)alphabet)[(ch ^ 85) & 63];
+	/* ----------- Function 2 ----------- */
 
-    /* ----------- Function 4 ----------- */
-    key[3] = ((char *)alphabet)[findLargest(argv[1], usernameLength)];
+	ch = index = 0;
 
-    /* ----------- Function 5 ----------- */
-    key[4] = ((char *)alphabet)[multiplyChars(argv[1], usernameLength)];
+	while (index < usernameLength)
+	{
+		ch = ch + argv[1][index];
+		index = index + 1;
+	}
+	key[1] = ((char *)alphabet)[(ch ^ 79) & 63];
 
-    /* ----------- Function 6 ----------- */
-    key[5] = ((char *)alphabet)[generateRandomChar(argv[1])];
-    key[6] = '\0';
+	/* ----------- Function 3 ----------- */
 
-    /* Print the generated key*/
-    for (ch = 0; key[ch]; ch++)
-        printf("%c", key[ch]);
+	ch = 1;
+	index = 0;
 
-    return (0);
+	while (index < usernameLength)
+	{
+		ch = argv[1][index] * ch;
+		index = index + 1;
+	}
+	key[2] = ((char *)alphabet)[(ch ^ 85) & 63];
+	
+	/* ----------- Function 4 ----------- */
+
+	key[3] = ((char *)alphabet)[findLargest(argv[1], usernameLength)];
+	/* ----------- Function 5 ----------- */
+
+	key[4] = ((char *)alphabet)[multiplyChars(argv[1], usernameLength)];
+	/* ----------- Function 6 ----------- */
+
+	key[5] = ((char *)alphabet)[generateRandomChar(argv[1])];
+	key[6] = '\0';
+
+	/* Print the generated key*/
+
+	for (ch = 0; key[ch]; ch++)
+		printf("%c", key[ch]);
+
+	return (0);
 }
-
