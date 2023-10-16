f9378f36d69819a3032282145647ad4fe890e2be
Ailwei <Ailweimaemu@gmail.com>
2023-10-16 06:06:45 -0700

    create _strncpy.c

---
+++ 0x18-dynamic_libraries/_strncpy.c
@@ -0,0 +1,26 @@
+#include "main.c"
+
+/**
+ * _strncpy - Copies at most 'n' bytes of a string from src to dest.
+ * @dest: The destination string.
+ * @src: The source string.
+ * @n: The maximum number of bytes to copy from src.
+ *
+ * Return: A pointer to the destination string.
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+	return (dest);
+}
