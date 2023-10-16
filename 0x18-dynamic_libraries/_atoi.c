abbc84fffcbbe307b90781641aa24b2588beab68
Ailwei <Ailweimaemu@gmail.com>
2023-10-16 05:28:54 -0700

    create _atoi.c

---
+++ 0x18-dynamic_libraries/_atoi.c
@@ -0,0 +1,33 @@
+#include "main.c"
+
+/**
+ * _atoi - Converts a string to an integer.
+ * @s: The string to be converted.
+ *
+ * Return: The integer value from the string.
+ */
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
+	int i = 0;
+
+	if (s[0] == '-')
+	{
+		sign = -1;
+		i++;
+	}
+	while (s[i] != '\0')
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			result = result * 10 + (s[i] - '0');
+		}
+		else
+		{
+			break;
+		}
+		i++;
+	}
+	return (result * sign);
+}
