b3169b76b8bc74fb5e9170eab69c02225b38aa38
Ailwei <Ailweimaemu@gmail.com>
2024-02-06 08:20:08 -0800

    haeser update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -10,5 +10,7 @@ int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
+int binary_searching(int *array, size_t low, size_t high, int value);
+void print_array(int *array, size_t low, size_t high);
 
 #endif
