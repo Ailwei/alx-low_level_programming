7f5c0f58228b5d8b6510935a0745ed54fcc21cf3
Ailwei <Ailweimaemu@gmail.com>
2024-02-06 12:26:36 -0800

    11. Advanced binary search

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,16 +1,14 @@
 #include "search_algos.h"
 
 /**
- * advanced_binary - Searches for a value in a
- * sorted array using advanced binary search
+ * advanced_binary - Searches for a value in a sorted array of integers
  * @array: Pointer to the first element of the array to search in
- * @size: Number of elements in the array
+ * @size: Number of elements in array
  * @value: Value to search for
  *
  * Return: The index where the value is located,
  * or -1 if not found or array is NULL
  */
-
 int advanced_binary(int *array, size_t size, int value)
 {
 	size_t i;
@@ -18,6 +16,7 @@ int advanced_binary(int *array, size_t size, int value)
 
 	if (array == NULL || size == 0)
 		return (-1);
+
 	printf("Searching in array: ");
 	for (i = 0; i < size; i++)
 	{
@@ -27,23 +26,25 @@ int advanced_binary(int *array, size_t size, int value)
 		else
 			printf("\n");
 	}
+
 	if (size == 1 && array[0] != value)
 		return (-1);
-	index = size / 2;
-	if (array[index] == value)
 
+	index = size / 2;
+
+	if (array[index] == value)
 	{
 		for (i = index; i > 0 && array[i - 1] == value; i--)
 		return (i);
 	}
+
 	if (array[index] < value)
 	{
-		index++;
-		return (advanced_binary(array + index, size - index, value) + index);
+		int result = advanced_binary(array + index, size - index, value);
+
+		return (result == -1 ? -1 : result + index);
 	}
-	else
-	{
-		return (advanced_binary(array, index, value));
-	}
-	return (-1);
+
+	/* Add handling for the case where array[index] > value */
+	return (advanced_binary(array, index, value));
 }
